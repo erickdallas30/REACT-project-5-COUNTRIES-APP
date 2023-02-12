1c41d1cef2c88d624837bc1bdb61fb3b736ec499
Erick Estrada <erickdallas30@gmail.com>
2023-02-12 12:06:31 -0600

    uploading project

---
+++ src/Components/StateModal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StateModal = () => {
+    return (
+        <div className="loading-modal">
+            <div className="loading-background" />
+            <div class="loader"></div>
+        </div>
+    )
+}
+
+export default StateModal
